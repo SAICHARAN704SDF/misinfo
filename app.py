766a6bcf1d0000548e715c8731ee78b38632fc6f
Charan Kailasa <saicharan704sdf@gmail.com>
2025-11-26 15:54:45 +0530

    Fix indentation of app.run inside if __name__ == '__main__'

--- app.py
+++ app.py
@@ -210,4 +210,4 @@ def report():
 if __name__ == "__main__":
     print("🚀 Starting TruthGuard API on http://127.0.0.1:5000")
     port = int(os.environ.get("PORT", 5000))
-app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=port)
